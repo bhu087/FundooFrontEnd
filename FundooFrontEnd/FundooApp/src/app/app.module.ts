13bb963b173113dcd5ba0a285ed49b6d18524e2f
bhu087 <bhu087@gmail.com>
2021-06-02 17:48:28 +0530

    [Bhushan] Refactor. Registration page UI completed

--- FundooFrontEnd/FundooApp/src/app/app.module.ts
+++ FundooFrontEnd/FundooApp/src/app/app.module.ts
@@ -3,7 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 import { RegistrationComponent } from './pages/registration/registration.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatButtonModule} from '@angular/material/button';
+import { FormControl, FormGroup } from '@angular/forms'
 
 @NgModule({
   declarations: [
@@ -12,7 +21,16 @@ import { RegistrationComponent } from './pages/registration/registration.compone
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatCardModule,
+    FormsModule,
+    MatSnackBarModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    FormControl, FormGroup
   ],
   providers: [],
   bootstrap: [AppComponent]
