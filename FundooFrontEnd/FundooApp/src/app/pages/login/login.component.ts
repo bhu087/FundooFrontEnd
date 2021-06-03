9b0ca63345f12c44c842fe5d3e22546d7af489e4
bhu087 <bhu087@gmail.com>
2021-06-03 14:58:46 +0530

    [Bhushan] Refactor. Validation form for Login page

--- FundooFrontEnd/FundooApp/src/app/pages/login/login.component.ts
+++ FundooFrontEnd/FundooApp/src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
-  
+  constructor(private formBuilder: FormBuilder) { }
+  loginForm= this.formBuilder.group({
+        email : ['', [Validators.required, Validators.email]],//,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
+        password: ['', [Validators.required]], // Validators.minLength(6), Validators.maxLength(20),Validators.pattern('^(?=\\D*\\d)(?=[^a-z]*[a-z])(?=[^A-Z]*[A-Z]).{6,20}$')]],
+  });
   ngOnInit(): void {
   }
-
+  get loggingForm() { return this.loginForm.controls; }
 }
